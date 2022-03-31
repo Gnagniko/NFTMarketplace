b38eea2e01aa81cd5ef8944c6ea62cddf76a3388
Bibobo12 <tino.gnagniko@gmail.com>
2022-03-31 15:23:08 +0200

    build folder created

---
+++ build/precache-manifest.95d340ce6af8cec2d2da77a35605893a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "873aae641c072c806abd",
+    "url": "/static/css/main.99038579.chunk.css"
+  },
+  {
+    "revision": "873aae641c072c806abd",
+    "url": "/static/js/main.873aae64.chunk.js"
+  },
+  {
+    "revision": "8586eb5c985422f55713",
+    "url": "/static/css/1.c4b39a2a.chunk.css"
+  },
+  {
+    "revision": "8586eb5c985422f55713",
+    "url": "/static/js/1.8586eb5c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "832938020dce706aa797c9c731f3b16b",
+    "url": "/index.html"
+  }
+];
