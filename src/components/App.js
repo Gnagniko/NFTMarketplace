fcf9486c5267c4db873502d7fbb90f32e4520cbd
Bibobo12 <tino.gnagniko@gmail.com>
2022-03-31 18:19:42 +0200

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,8 @@ class App extends Component {
     }
 
     async loadBlockchainData() {
-        const web3 = new Web3(window.ethereum)
+        window.web3 = new Web3(window.ethereum)
+        const web3 = window.web3
         const accounts = await web3.eth.getAccounts()
         console.log(accounts[0])
         this.setState({account: accounts[0]})
