3bfca499895c56aeaf1347072686324b0bb7a129
Koffi Tino Gnagniko <49998663+Gnagniko@users.noreply.github.com>
2022-05-03 13:15:22 +0200

    Update TeamCards.js

--- src/components/TeamCards.js
+++ src/components/TeamCards.js
@@ -18,7 +18,7 @@ function TeamCards (){
                     <div class="bg-white rounded shadow-sm "><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
                     <h5 class="mb-0">Koffi Tino Gnagniko</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
                     <hr></hr>
-                    <abbr title="Telefonnummer">Tel. +49 1573 0056780</abbr> <br></br>
+                    <abbr title="Telefonnummer">Tel. +49 15172152014</abbr> <br></br>
                     <abbr title="Email-Adresse">Email: tino.gnagniko@gmail.com</abbr> <a></a>
                     <ul class="social mb-0 list-inline mt-3">
                         <li class="list-inline-item"><a href="https://discord.gg/CTrwTaVW" class="social-link"><BsDiscord/></a></li>
